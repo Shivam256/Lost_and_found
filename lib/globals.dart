0c16e1bc4c03e47d203d5e28995c9242920bc811
shivam256 <shivamgavandi256@gmail.com>
2022-04-07 17:26:40 +0530

    item page ui completed

--- lib/globals.dart
+++ lib/globals.dart
@@ -3,13 +3,19 @@ import 'package:flutter/material.dart';
 const kTextStyle1 = TextStyle(
   fontSize: 30,
   color: Color(0xFF2E3E5C),
-  fontWeight: FontWeight.w700,
+  fontWeight: FontWeight.w500,
 );
 
 const kTextStyle2 = TextStyle(
   fontSize: 18,
   color: Color(0xFF9FA5C0),
   fontWeight: FontWeight.w500,
+);
+
+const kTextStyle3 = TextStyle(
+  fontSize: 18,
+  color: Color(0xFF979797),
+  fontWeight: FontWeight.w300,
 );
 
 const primaryColor = Color(0xFF6C63FF);
--- lib/screens/foundItem_screen.dart
+++ lib/screens/foundItem_screen.dart
@@ -3,6 +3,8 @@ import 'package:lost_and_found/components/custom_button.dart';
 import 'package:lost_and_found/globals.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class FoundItemScreen extends StatelessWidget {
   const FoundItemScreen({Key? key}) : super(key: key);
 
@@ -30,8 +32,79 @@ class FoundItemScreen extends StatelessWidget {
               );
             }).toList(),
           ),
-          Text('boAt Headphones',style: kTextStyle1,),
-          Text('Found 8 hours ago',style: kTextStyle2,)
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'boAt Headphones',
+                  style: kTextStyle1,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Found 8 hours ago',
+                  style: kTextStyle2,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                      margin: EdgeInsets.only(right: 20),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: primaryColor),
+                      child: Text(
+                        '#headphones',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: primaryColor),
+                      child: Text(
+                        '#headphones',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at velit sed sem laoreet fringilla non ut erat. Nulla mollis finibus imperdiet. Sed vel enim vitae lorem dignissim sagittis nec ut nunc. Vivamus placerat rutrum velit sed feugiat. Pellentesque sed sagittis nibh. Ut eget fermentum dolor',
+                  style: kTextStyle3,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Location',
+                  style: kTextStyle1,
+                ),
+                     SizedBox(
+                  height: 10,
+                ),
+                Image(
+                  image: NetworkImage('https://i.stack.imgur.com/HILmr.png'),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                CustomButton(buttonText: 'CLAIM', onClick: () {}),
+              ],
+            ),
+          )
         ],
       ),
     );
